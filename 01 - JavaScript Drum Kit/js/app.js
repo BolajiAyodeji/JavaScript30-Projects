f98810c1c64ba97c5aa804c983065567a37e587f
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-14 11:35:55 +0100

    update

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,13 +1,18 @@
-window.addEventListener('keydown', function (e) {
+window.addEventListener('keydown', );
+
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; //Stop function from running together
   audio.currentTime = 0; //Rewind sound
   audio.play();
   key.classList.add('playing');
-  setTimeout(function () {
+}
 
-    }, 0.6
-  }
-
-});
+function removeTransition(e) {
+  if (e.propertyName !== 'transform')
+    return; // skip if its not a transform
+  this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
