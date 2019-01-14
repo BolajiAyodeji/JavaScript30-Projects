d9ae7fb0bb187bff8d2fbc56875b0f58822aa9fc
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-14 03:53:03 +0100

    rewing F

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!audio) return; //Stop function from running together
-
+  audio.currentTime = 0; //Rewind sound
   audio.play();
+
 });
