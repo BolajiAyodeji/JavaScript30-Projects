4d20d6e65b5cb4bebd752a6547f46028833a4cfd
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-14 10:56:33 +0100

    set timeoutA

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,7 +1,13 @@
 window.addEventListener('keydown', function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; //Stop function from running together
   audio.currentTime = 0; //Rewind sound
   audio.play();
+  key.classList.add('playing');
+  setTimeout(function () {
+
+    }, 0.6
+  }
 
 });
